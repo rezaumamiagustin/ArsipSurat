209aecefea919f3d12cfae5911ad5d8086df2020
Reza Umami <60643423+rezaumamiagustin@users.noreply.github.com>
2021-10-24 07:56:27 +0700

    finish

---
+++ application/views/about.php
@@ -0,0 +1,24 @@
+<?php $this->load->view('layouts/header'); ?>
+
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" style="margin-top: 20px;">About</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <img src="<?= base_url(); ?>assets/img/pict.jpg" alt="" width="250px">   
+            </div>
+            <div class="col-lg-9 col-md-6">
+               <h3>Website ini dibuat oleh:</h3><br>
+               <b>Nama : Reza Umami Agustin <br>
+               NIM : 1831710145 <br>
+               Tanggal : 23 oktober 2021</b>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php $this->load->view('layouts/footer'); ?>
